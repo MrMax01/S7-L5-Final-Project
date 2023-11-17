639c27c64488527d488cbfcf8d93a2e156059836
MrMax01 <massimo.mauro00@gmail.com>
2023-11-17 11:20:22 +0100

    first commit

---
+++ src/main/java/massimomauro/S7L5FinalProject/S7L5FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package massimomauro.S7L5FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L5FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L5FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/massimomauro/S7L5FinalProject/S7L5FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package massimomauro.S7L5FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L5FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
